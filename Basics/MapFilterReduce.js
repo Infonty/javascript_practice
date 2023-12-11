24ec07a52add9378653ee0b7597b1ab16fd0b34e
Infonty <97352140+Infonty@users.noreply.github.com>
2023-12-11 12:37:53 +0530

    Update MapFilterReduce.js

--- Basics/MapFilterReduce.js
+++ Basics/MapFilterReduce.js
@@ -12,9 +12,38 @@ undefined
 console.log(arr)
 VM2135:1 (4) [3, 6, 1, 4]
 undefined
-const outpt3=arr.reduce((accumuletor,current)=>{
+const outpt3=arr.reduce((accumuletor,current)=>{        //accumletor as a sum and it is used to find sum, min and max in array
     accumuletor+=current;
     return accumuletor;
 },0)
 console.log(outpt3)
-VM2374:5 14
+10
+
+//
+const obj=[
+    {firstname:"MOhit" ,lastname:"singh" ,age:22 },
+    {firstname:"polhit" ,lastname:"Radha" ,age:22 },
+    {firstname: "Rhhoit",lastname:"Kuntal" ,age:24 },
+    {firstname:"banu" ,lastname:"gay" ,age: 15},
+    {firstname:"babuuuuu" ,lastname:"Daya" ,age: 15}
+]
+
+//select first and last name
+const outpt=obj.map((x)=>x.firstname+" "+x.lastname);
+console.log(outpt)
+
+//group them in terms of age like {22:1,20:2,15:2}
+
+const otpt=obj.reduce((acc,curr)=>{
+    if(acc[curr.age]){
+        acc[curr.age]=++acc[curr.age];
+    }
+    else {
+        acc[curr.age]=1
+    }
+    return acc
+},{});
+console.log(otpt)
+//to print firstname having age > 20
+const ot=obj.filter((x)=>x.age>20).map((x)=>x.firstname)
+console.log(ot)
