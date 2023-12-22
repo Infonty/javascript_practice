09677005657c3731a2e9d92075dd5fe6c02f3733
Infonty <97352140+Infonty@users.noreply.github.com>
2023-12-22 18:58:47 +0530

    Create producerOf_promises.js

---
+++ Basics/producerOf_promises.js
@@ -0,0 +1,30 @@
+
+const cartt=["shoes","pants","shirt","jacket","Mobile"];
+
+const promisee=createOrder(cart);
+
+promisee.then(function(orderId){
+    proceedToPayment(orderId);
+});
+//Producer
+function createOrder(cartt){
+    const pr=new Promise(function(resolve,reject){  //having resolve and reject fnc 
+        //createOrder
+        //validateCart
+        //orderId
+        if(!validateCart(cartt)){
+            const err=new Error("cart is invalid")
+            reject(err);
+        }
+
+        const orderId="12345";
+        resolve(orderId);
+    });
+
+
+    return pr;
+
+}
+function validateCart(cartt){
+    return  true;
+}
